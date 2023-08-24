406ca6be735fc468b2f695fbfd2a47d0ea876e41
1fbifbi23 <danii.llarionov111@gmail.com>
2023-08-25 04:11:36 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -30,3 +30,10 @@ function ClearAll(){
     inputDate.value = '';
     day.innerHTML = '';
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 1000);
